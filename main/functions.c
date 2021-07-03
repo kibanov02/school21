9dcb01e53f5e299d499fbd8917229a911aad5692
lopin <52812290+kibanov02@users.noreply.github.com>
2021-07-03 23:05:04 +0300

    Add files via upload

---
+++ main/functions.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *) s1 - *(unsigned char *) s2);
+}
---
+++ main/main00.c
@@ -0,0 +1,10 @@
+#include "ft.h"
+#include <stdio.h>
+int main()
+{
+	char a[] = "This is a string";
+	printf("%d\n", ft_strlen(a));
+	ft_putstr(a);
+	printf("\n");
+	ft_putchar('g');
+}
