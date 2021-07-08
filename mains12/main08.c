98ef5355c3e1688de7dc2fb355b5a9ba9d73ad7c
lopin <52812290+kibanov02@users.noreply.github.com>
2021-07-08 22:55:01 +0300

    Add files via upload

--- mains12/main08.c
+++ mains12/main08.c
@@ -1,10 +1,11 @@
+#include "../ex00/ft_create_elem.c"
 #include "../ex05/ft_list_push_strs.c"
 #include "../ex08/ft_list_reverse.c"
 #include <stdio.h>
 
 int	main(void)
 {
-	char	*arr[1];
+	char	*arr[3];
 
 	char	*s1 = "Last line\n";
 	char	*s2 = "Second line\n";
@@ -12,7 +13,7 @@ int	main(void)
 	arr[0] = s1;
 	arr[1] = s2;
 	arr[2] = s3;
-	t_list	*res = ft_list_push_strs(1, arr);
+	t_list	*res = ft_list_push_strs(3, arr);
 	ft_list_reverse(&res);
 	while (res)
 	{
